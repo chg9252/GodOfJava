94cd6aeb37235a998fde4f208dc7183295234ea6
Naerok <chg9252@gmail.com>
2022-12-07 22:09:16 +0900

    기본 생성자

---
+++ src/main/java/act8/ReferenceDefault.java
@@ -0,0 +1,7 @@
+package act8;
+
+public class ReferenceDefault {
+    public static void main(String[] args) {
+        ReferenceDefault referenceDefault = new ReferenceDefault();
+    }
+}
---
+++ src/main/java/act8/ReferenceString.java
@@ -0,0 +1,13 @@
+package act8;
+
+public class ReferenceString {
+
+    public ReferenceString(){}
+    public ReferenceString(String arg){}
+
+    public static void main(String[] args) {
+        ReferenceString reference = new ReferenceString();
+    }
+
+    // 기본 생성자는 다른 생성자가 있으면 자동으로 생성하지 않는다.
+}
